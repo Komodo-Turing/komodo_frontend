01762ed2bab1b8f6ba1ea8ebb6c64a1e5ac2e631
Stephanie Helm <stephaniemhelm@gmail.com>
2022-01-06 14:15:57 -0700

    navbar

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+
+  def create
+
+    auth_hash = request.env['omniauth.auth']
+
+    name = auth_hash["info"]["name"]
+    email = auth_hash["info"]["email"]
+    token = auth_hash["credentials"]["token"]
+    google_id = auth_hash["uid"]
+
+
+    # user = User.find_or_create_by(email: email)
+    # user.update(name: name, email: email, token: token, google_id: google_id)
+
+    # session[:user_id] = user.id
+
+    #redirect_to '/dashboard'
+  end
+end
