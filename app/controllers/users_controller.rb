bdc49c4c5f07f266f7d43f9f505fd46581d5479e
Haewon Jeon <haewonito@gmail.com>
2022-01-06 20:26:14 -0700

    session stuff again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,17 +17,31 @@ class UsersController < ApplicationController
       redirect_to '/dashboard'
 
     else #if user is new and doesn't have a phone number, take them to update page
-      redirect_to '/user/edit'
+      redirect_to '/users/edit'
     end
   end
 
-  def new
+  def show
+    user_id = session[:user_id]
+    @user = User.find(user_id)
+  end
+  
+  def edit
   end
 
   def update
+    auth_hash = request.env['omniauth.auth']
 
+    name = auth_hash["info"]["name"]
+    email = auth_hash["info"]["email"]
+    token = auth_hash["credentials"]["token"]
+    google_id = auth_hash["uid"]
+
+    user = User.create(name: name, email: email, token: token, google_id: google_id, phone_number: params[:phone_number])
+
+    session[:user_id] = user.id
+    redirect_to '/dashboard'
   end
 end
 
 # don't forget phone number and address
-
