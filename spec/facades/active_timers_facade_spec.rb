833d4a98bee9656c9e718226f9fe9f29fdac57b6
Micha Bernhard <mib70@pitt.edu>
2022-01-09 17:03:24 -0700

    Test: test active timers facade

---
+++ spec/facades/active_timers_facade_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Active Timers Facades' do
+  it 'creates an instance of an Active Timer' do
+    active_timer_params = { 
+      user_id: 1,
+      name: 'Timer',
+      duration: 120,
+      start_time: DateTime.now,
+      substance: 'Drug',
+      dosage: '10oz',
+      entry_instructions: 'The building code is 1234',
+      notes: 'These are some notes',
+      status: 'Active'
+    }
+    
+    expect(ActiveTimersFacade.create_active_timer(active_timer_params)).to be_an ActiveTimer
+  end
+end
