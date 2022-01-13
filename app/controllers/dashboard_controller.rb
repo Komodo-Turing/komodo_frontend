f9f6d2a4c0d16835ebfe1d4197e8f70cb18e855e
Stephanie Helm <stephaniemhelm@gmail.com>
2022-01-13 15:59:22 -0700

    show spec

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,7 +6,5 @@ class DashboardController < ApplicationController
     @contacts = ContactsFacade.get_contacts(user_id)
     @timers = TimersFacade.get_timers(user_id)
   end
-  #I think the easiest solution to limit to 5 contact would be to
-  #just return the last five contact, and if there's more than 5 in
-  #the database, we will check here and give a warning flash
+  
 end
